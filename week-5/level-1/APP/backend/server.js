8ed010972783272d935925f07daf899c49982317
Lohith V P <66241468+lohith28-bit@users.noreply.github.com>
2024-05-15 16:41:09 +0530

    Updated server.js

--- week-5/level-1/APP/backend/server.js
+++ week-5/level-1/APP/backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const { checkType } = require('./type');
+const { checkType, checkID } = require('./type');
 const { card_data } = require('./db');
 const app = express();
 
@@ -10,9 +10,7 @@ app.use(express.json())
 app.post('/create', async (req, res) => {
 	try {
 		const createPayload = req.body;
-		console.log(createPayload)
-		const checkAuth = checkType.parse(createPayload);
-		console.log(checkAuth)
+		const checkAuth = checkType.safeParse(createPayload);
 		if (!checkAuth) {
 			res.status(411).json({
 				msg: "Invalid input formate"
@@ -32,14 +30,64 @@ app.post('/create', async (req, res) => {
 })
 
 
-app.get('/create', (req, res) => {
-
+app.get('/get_data', async (req, res) => {
+    try{
+        const data = await card_data.find();
+        res.status(200).json({
+            card_data : data
+        })
+    }catch(err){
+        res.status(411).json({
+            msg : err.message
+        })
+    }
 })
-app.put('/create', (req, res) => {
 
+app.put('/update', async (req, res) => {
+    try{
+        const card_id_payload = req.body;
+        const check_card_id = checkID.safeParse(card_id_payload)
+        if(!check_card_id){
+            res.status(411).json({
+                msg : "Something went wrong"
+            })
+            return
+        }
+        const data_title =["Name","Description","Interest","LinkedIn","Twitter"]
+        const changingTitle = data_title[req.query.title_idx]
+        const updatingObject = {}
+        console.log(updatingObject)
+        updatingObject[changingTitle] = req.query.title
+        const data = await card_data.findOneAndUpdate({
+            "_id" : card_id_payload.id,
+        },updatingObject,{new: true})
+
+        res.status(200).json({
+            msg: "Card Updated successfully",
+            'card_data' : data
+        })
+    }catch(err){
+        res.status(411).jsonb({
+            msg : err.message
+        })
+    }
 })
-app.delete('/create', (req, res) => {
+app.delete('/delete', async (req, res) => {
+    try{
+        const ID_payload = req.query.id;
+        const data = await card_data.deleteOne({
+            '_id': ID_payload.id
+        })
 
+        res.status(200).json({
+            msg : "Card deleted successfully",
+            card : data 
+        })
+    }catch(err){
+        res.status(411).json({
+            msg : err.message
+        })
+    }
 })
 
 app.listen(3000, () => {
