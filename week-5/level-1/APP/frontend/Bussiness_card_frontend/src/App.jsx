5b78611c04158ff4141002cc5309451960dbe9ac
Lohith V P <66241468+lohith28-bit@users.noreply.github.com>
2024-05-16 20:08:40 +0530

    Update App.jsx

--- week-5/level-1/APP/frontend/Bussiness_card_frontend/src/App.jsx
+++ week-5/level-1/APP/frontend/Bussiness_card_frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ShowData from './components/ShowData';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: ErrorPage
+  },
+  {
+    path: '/showdata',
+    element: <ShowData />,
+    errorElement: ErrorPage
+  },
+]);
 
 
 function App() {
-
   return (
     <>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
